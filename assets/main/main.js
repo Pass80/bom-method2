87754e4801419ca48a6b07e9605f3ba375529781
Feras <ferassaleh1980@gmail.com>
2022-11-22 00:12:54 +0100

    m

---
+++ assets/main/main.js
@@ -0,0 +1,15 @@
+const display = document.querySelector('.zeit');
+const btn = document.querySelector('#btn');
+let percentage = 100;
+
+btn.addEventListener('click', () => {
+    const decrease = () => {
+        if (percentage < 0) {
+            clearInterval(timer);
+        } else {
+            display.innerHTML = `${percentage}%`;
+            percentage -= 1;
+        }
+    };
+    const timer = setInterval(decrease, 100);
+});
